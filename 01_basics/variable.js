f43ef5097b1fbd137ff0b7f2753a073fc6b49633
keshavwalia-ctrl <keshav.walia@magicedtech.com>
2026-07-24 11:43:40 +0000

    data types

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const account_id=12123
+let accountemail="waliakeshav900@gmail.c"
+var accountcity="jaipur"
+let accountpassword="!22121"
+
+accountemail="xyz@gmail.com"
+accountcity="delhi"
+accountpassword="2323"
+
+console.table([account_id,accountcity,accountemail,accountpassword])
