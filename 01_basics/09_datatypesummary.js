913d4f636e11480dbabc0bc2a08838c4b7c013ef
keshavwalia-ctrl <keshav.walia@magicedtech.com>
2026-07-24 20:32:10 +0000

    datatypes

---
+++ 01_basics/09_datatypesummary.js
@@ -0,0 +1,27 @@
+const id= Symbol('123')
+const anthorid= Symbol('123')
+
+console.log(id)
+console.log(anthorid)
+
+
+
+// Array
+
+const sims=["airtel","jio","vodaphone","bsnl"]
+
+// object
+
+let obj={
+    name:"keshav",
+    age:20
+
+}
+
+// functions
+
+const myfun = function(){
+    console.log("helloworld"); 
+}
+
+console.log(myfun)
