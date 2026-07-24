e141cf85bed040369d79ab4d7d433990f8cff382
keshavwalia-ctrl <keshav.walia@magicedtech.com>
2026-07-24 17:54:38 +0000

    coneversion of data types

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,22 @@
+let score = "abc"
+let valueinnumber= Number(score)
+
+console.log(typeof valueinnumber) 
+console.log(valueinnumber)
+
+let loogedin ="hitesh"
+
+let booleanisloggedin =Boolean(loogedin) 
+
+console.log(booleanisloggedin);
+
+let a = "hites"
+
+let b=Boolean(a)
+
+console.log(b);
+
+
+ let somenum = 233
+ let striingnum =String(somenum)
+ console.log(typeof striingnum)
