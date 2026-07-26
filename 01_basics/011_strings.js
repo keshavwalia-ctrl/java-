1fa2bae13cb951447f7a1c87abda47b5e246eef2
keshavwalia-ctrl <keshav.walia@magicedtech.com>
2026-07-26 13:12:39 +0000

    strings

---
+++ 01_basics/011_strings.js
@@ -0,0 +1,20 @@
+let name="keshav"
+let repcount =50
+
+console.log(`my name is ${name}and my rep count is ${repcount}`);  
+
+
+
+const gamename = new String('keshavic')
+
+console.log(gamename)
+
+console.log(gamename[0]) // to find key value
+
+console.log(gamename.__proto__)  // tell the object values
+
+console.log(gamename.length)
+
+console.log(gamename.toLocaleUpperCase())
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf("i"))
