e477091e1d32eb67075ccfbefdae1d294128b7f9
keshavwalia-ctrl <keshav.walia@magicedtech.com>
2026-07-26 19:49:30 +0000

    string

--- 01_basics/011_strings.js
+++ 01_basics/011_strings.js
@@ -5,7 +5,7 @@ console.log(`my name is ${name}and my rep count is ${repcount}`);
 
 
 
-const gamename = new String('keshavic')
+const gamename = new String('keshavic new game ')
 
 console.log(gamename)
 
@@ -18,3 +18,27 @@ console.log(gamename.length)
 console.log(gamename.toLocaleUpperCase())
 console.log(gamename.charAt(2));
 console.log(gamename.indexOf("i"))
+console.log(gamename.substring(0,6)) 
+// in the sub string   the last value is not counted
+ 
+
+
+const anthorstring= gamename.slice(-8,4)
+
+console.log(anthorstring)
+
+const newstring= "   hitesh   "
+console.log(newstring.trim());
+// removes the unwanted space 
+
+const url = "https://keshav.com/keshav%20walia"
+
+console.log(url.replace("%20","-"))
+// replace the word in the string 
+
+const a = "keshavewalia"
+
+console.log(a.includes("wal"))
+// to find that the word exist in the string 
+
+console.log(gamename.split("-"))
