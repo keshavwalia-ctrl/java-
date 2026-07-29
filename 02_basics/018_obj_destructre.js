145fc79d10f1eaa426ccae52978897de4b69bbe2
keshavwalia-ctrl <keshav.walia@magicedtech.com>
2026-07-29 11:18:23 +0000

    objects

---
+++ 02_basics/018_obj_destructre.js
@@ -0,0 +1,17 @@
+const course={
+    coursename:"js tutorial",
+    price:"999",
+    courseinstructer:"hitesh"
+}
+const {courseinstructer:instructer} = course 
+// console.log(courseinstructer)
+
+// const{courseinstructer} is used so we can 
+// get the single output 
+console.log(instructer)
+
+{
+    "name":"keshav",
+    "age":"20",
+}
+// this is a json  
